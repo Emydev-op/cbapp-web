f67d38f48625b38088f5024074bb22334a418908
Emydev-op <alphaoseghe@gmail.com>
2024-05-18 17:48:52 +0100

    complete of home feature 2

--- src/components/Home/Feature2.jsx
+++ src/components/Home/Feature2.jsx
@@ -12,10 +12,15 @@ export default function Feature2() {
     { name: "Dropbox", brand: "Cloud", price: "$10" },
   ];
   return (
-    <div className="bg-[var(--blue-500)] flex justify-center z-[-2]">
+    <div className="bg-[var(--blue-500)] flex justify-center">
       <div className="pt-28 md:flex md:justify-between md:max-w-screen-lg mb-24">
         <div className="md:w-[54%] w-full space-y-10">
-          <h2 className="font-md text-6xl">Send & receive money instantly</h2>
+          <h2 className="font-md text-6xl">
+            Send & receive money{" "}
+            <span className="bg-gradient-to-r blur-xs from-blue-500 via-teal-500 to-orange-500 bg-clip-text text-transparent text-center select-none">
+              instantly
+            </span>
+          </h2>
           <p className="font-md text-[20px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et.
@@ -90,7 +95,7 @@ export default function Feature2() {
           </ul>
         </div>
         <div className="md:mt-0 md:block md:w-[30%] relative">
-          <div className="absolute w-full z-[-1]">
+          <div className="absolute w-full ">
             <div className="flex flex-col gap-3">
               {payment?.map((item) => (
                 <div
