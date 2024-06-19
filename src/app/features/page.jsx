0164739570c72b4579bc9c38b372118cecb82135
Emydev-op <alphaoseghe@gmail.com>
2024-06-19 13:01:43 +0100

    update on the feature

--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -4,17 +4,17 @@ import React from "react";
 
 export default function FeaturePage() {
   return (
-    <section className="relative w-full md:h-[calc(100dvh-96px)]">
+    <section className="relative w-full h-[50dvh] md:h-[calc(100dvh-96px)]">
       <div className="bg-[var(--blue-500)] -z-10 absolute top-0 left-0 right-0 bottom-0" />
-      <div className="text-center font-medium tracking-[-3%] max-w-lg mx-auto space-y-2 pt-12 relative">
+      <div className="text-center font-medium tracking-[-3%] max-w-lg mx-auto space-y-2 pt-20 md:pt-12 px-6 md:px-0 relative">
         <h5 className=" text-xl">Features</h5>
         <h1 className=" text-5xl md:text-7xl pb-2">All in one card.</h1>
-        <p className="text-xl ">
+        <p className="md:text-xl text-lg">
           Senectus et netus et malesuada fames ac turpis. Sagittis vitae et leo
           duis ut diam.
         </p>
-        <div className=" inline-flex gap-3 md:pt-4">
-          <button className="bg-[var(--blue-600)] text-white rounded-md px-5 py-3">
+        <div className=" inline-flex gap-3 pt-4">
+          <button className="bg-[var(--blue-600)] text-white rounded-md px-4 md:px-5 py-2 md:py-3">
             Open Account
           </button>
           <button
@@ -38,7 +38,7 @@ export default function FeaturePage() {
           </button>
         </div>
       </div>
-      <div className="absolute -z-10 -bottom-20">
+      <div className="absolute w-full -z-10 bottom-0 md:-bottom-20">
         <Image
           src={"/borderCards.svg"}
           alt="cbpay-card"
